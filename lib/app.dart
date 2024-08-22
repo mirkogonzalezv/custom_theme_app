4a7d2ef5c48b468729c078a95a47c27740f99a3e
Mirko Gonzalez Vasquez <mirko.gonzalez@xintec.cl>
2024-08-22 17:32:09 -0400

    wip: ejemplo de estilo listo

--- lib/app.dart
+++ lib/app.dart
@@ -7,14 +7,93 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Custom theme',
       theme: appCustomTheme,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Custom theme app'),
+          title: const Text('Custom Theme'),
         ),
-        body: const Center(
-          child: Text('Ejemplo de estilo custom'),
+        body: Column(
+          children: [
+            SizedBox(
+              width: double.maxFinite,
+              child: Card(
+                child: Container(
+                  margin: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      const Text(
+                        "Titulo de la card",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                        ),
+                      ),
+                      const Text("Contenido de la Card"),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      const Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 36,
+                            backgroundImage: NetworkImage('https://picsum.photos/200'),
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  top: 0,
+                                  left: 0,
+                                  child: Icon(
+                                    Icons.person,
+                                    size: 60,
+                                    color: Colors.deepPurple,
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.person,
+                                  size: 55,
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            width: 14,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Titulo imagen",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "Contenido a la derecha",
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      SizedBox(
+                        width: double.maxFinite,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text("Click here!"),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
--- lib/common/theme/app_custom_theme.dart
+++ lib/common/theme/app_custom_theme.dart
@@ -2,18 +2,41 @@ import 'package:flutter/material.dart';
 
 final appCustomTheme = ThemeData(
   colorScheme: ColorScheme.fromSeed(
-    seedColor: Colors.deepPurple,
+    seedColor: Colors.green,
     brightness: Brightness.dark,
   ),
 
   /// AppBar Style Theme
   appBarTheme: const AppBarTheme(
-    backgroundColor: Colors.deepPurple,
+    // backgroundColor: Colors.deepPurple,
     centerTitle: true,
   ),
 
   /// FloatingAction Button Theme
   floatingActionButtonTheme: const FloatingActionButtonThemeData(
-    backgroundColor: Colors.deepPurple,
+      // backgroundColor: Colors.deepPurple,
+      ),
+
+  /// Card Theme
+  cardTheme: CardTheme(
+    // color: Colors.deepPurple.shade200, // Color global par atodas las cards
+    elevation: 4,
+    margin: const EdgeInsets.all(20), // Margen global para todas las cards
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(20), // Aumentamos el border radius global de todas las cards
+    ),
+    // shadowColor: Colors.deepPurple.shade300, // Color de  la sombra de todas las crads
+    clipBehavior: Clip.antiAlias,
+    // surfaceTintColor: Colors.deepPurple.shade200, // Color de la superficie de la tarjeta, sobre el color principal
+  ),
+
+  /// Elevated Button Theme
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      // backgroundColor: Colors.deepPurple, // Todos los botones tendran este color
+      elevation: 2,
+      enableFeedback: true,
+      // foregroundColor: Colors.deepPurple.shade200, // Color del texto dentro del botón
+    ),
   ),
 );
